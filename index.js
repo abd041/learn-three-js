12c677a29bbea946d64dc9abfd966843bd69da33
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2026-01-02 19:13:33 +0500

    first lecture completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {OrbitControls} from "jsm/controls/OrbitControls.js"
 
 // Created a render and set it
 
@@ -15,17 +16,36 @@ camera.position.z = 2;
 // Create and scene and add object inside it
 
 const geometry = new THREE.IcosahedronGeometry(1 , 2);
-const material = new THREE.MeshBasicMaterial({
-    color:0x00ccff
+const material = new THREE.MeshStandardMaterial({
+    color:0xffffff,
+    flatShading:true,
 });
 
-const mesh = new THREE.Mesh(geometry,material);
+const wireMat = new THREE.MeshBasicMaterial({
+    color:0xffffff,
+    wireframe:true,
+})
+
+const controls = new OrbitControls(camera , renderer.domElement)
+controls.enableDamping =true
+controls.dampingFactor = 0.03;
+const mesh = new THREE.Mesh(geometry , material);
+const wirematmesh = new THREE.Mesh(geometry,wireMat)
+mesh.add(wirematmesh)
 const scene = new THREE.Scene();
+
+const hemiLight =  new THREE.HemisphereLight(0x00ffff, 0xff00ff, 1.5);
+scene.add(hemiLight)
 
 scene.add(mesh);
 
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene , camera)
+    controls.update()
+}
 
-renderer.render(scene , camera)
+animate()
 
 
 
